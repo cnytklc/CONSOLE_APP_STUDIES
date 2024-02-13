6673c2549fb8ffa9a7f290fbc9ff3ebf6eed082e
Cüneyt Kılıç <cnytklc10@gmail.com>
2024-02-14 02:52:05 +0300

    HelloWorld and StudentID codes are created

---
+++ consoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿//Hello World and Student ID
+Console.WriteLine("Hello, World!");
+
+Console.Write("Enter Student Name:");
+var studentName = Console.ReadLine();
+Console.Write("Enter Student Surname:");
+var studentSurname = Console.ReadLine();
+Console.Write("Enter Student Number:");
+int studentNo = Convert.ToInt32(Console.ReadLine());
+
+
+    switch (studentNo)
+    {
+        case 190403084:Console.WriteLine($"{studentName} {studentSurname}");
+        break;
+
+        default:Console.WriteLine("THERE IS NO STUDENT NUMBER WHICH YOU ENTER");
+        break;
+    }
+    
