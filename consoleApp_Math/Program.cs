3ba2236c43f2452d234b206083c30e2c43a792a8
Cüneyt Kılıç <cnytklc10@gmail.com>
2024-02-15 02:45:09 +0300

    console app. math operation is created

---
+++ consoleApp_Math/Program.cs
@@ -0,0 +1,33 @@
+﻿// MATH OPERATIONS
+
+var number= 4.5;
+
+var result = Math.Ceiling(number);
+var result1 = Math.Round(number);
+var result2 = Math.Floor(number);
+var result3 = Math.Min(5,2);
+var result4 = Math.Max(5,2);
+var result5 = Math.Abs(-5);
+var result6 = Math.Cbrt(50);
+var result7 = Math.Exp(2);
+
+
+//////////////////////
+///Normal code of max and min finder
+// MAX-MIN
+Console.WriteLine("Enter 3 number between 1-500 :");
+List<int>num = new List<int>();
+var bigNum = 0;
+var minNum = 501;
+for (int i = 0; i <= 2; i++)
+{
+var nums = Convert.ToInt32(Console.ReadLine());
+num.Add(nums);
+if(num[i]>bigNum){
+    bigNum=num[i];
+};
+if(num[i]<minNum){
+    minNum=num[i];
+};
+}
+Console.Write($"Big num is = {bigNum} and minNum = {minNum}");
