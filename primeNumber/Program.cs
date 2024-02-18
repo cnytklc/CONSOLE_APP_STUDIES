79d9c899d72871f36710c2a830c74b7649c6f0b2
Cüneyt Kılıç <cnytklc10@gmail.com>
2024-02-19 01:26:35 +0300

    Prime Number Find Code

---
+++ primeNumber/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Enter a number: ");
+var num1 =  Convert.ToInt32(Console.ReadLine());
+var oldNum = 0;
+int newNum = 1;
+var result = 0;
+for (int i = 0; i < num1; i++)
+{
+Console.Write($"{oldNum} ");
+
+   result = newNum + oldNum;
+   oldNum=newNum;
+ newNum = result;
+
+
+}
