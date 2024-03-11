168a396aca963cdd1963983fafeefc2901b65757
Cüneyt Kılıç <cnytklc10@gmail.com>
2024-03-11 19:27:02 +0300

    Arrays, Strings, and Examples are uploaded

---
+++ CSharpCourse/Arrays/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {//ONE DIMENSION ARRAYS
+         //int[] numbers = new int[5];
+         //numbers[0] = 1; 
+         //numbers[1] = 2;
+         //numbers[2] = 3;
+         //numbers[3] = 4;
+         //numbers[4] = 5;
+
+            //foreach (int i in numbers)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //Console.ReadLine();
+
+            //string[] names = { "ALİ", "AHMET", "HASAN", "HÜSEYİN" };
+
+            //foreach(string name in names)
+            //{
+            //    Console.WriteLine(name);
+            //}
+            //Console.ReadLine();
+
+            ///////////////////////////////////////////////////////////////////////////////
+
+            // MORE THAN 1 DIMENTIONS
+
+            //string[,] citys = new string[2, 3]
+            //{
+            //    {"Istanbul","Izmir","Ankara" },
+            //    {"Manisa","Antalya","Ağrı" }
+            //};
+
+            //for (int i = 0; i <= citys.GetUpperBound(0); i++)
+            //{
+            //    for (int j = 0; j <= citys.GetUpperBound(1); j++)
+            //        Console.WriteLine(citys[i,j]);
+            //}
+            //Console.ReadLine();
+
+
+            ////////////////////////////////////////////////////////////////////////////
+
+
+            // EXAMPLE OF DUBLICATE LETTER IN A WORD 
+           // DublicateLetter();
+
+        }
+
+        private static void DublicateLetter()
+        {
+            List<char> notDublicate = new List<char>();
+            List<char> dublicate = new List<char>();
+
+            Console.Write("Enter a word: ");
+            var word = Console.ReadLine();
+
+            foreach (var letter in word)
+            {
+                if (!notDublicate.Contains(letter))
+                {
+                    notDublicate.Add(letter);
+                }
+                else
+                {
+                    dublicate.Add(letter);
+                }
+            }
+            Console.Write("Dublicated letter is: ");
+            foreach (var dub in dublicate)
+            {
+                Console.Write(dub);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CSharpCourse/asalSayı/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asalSayı
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            var number = Convert.ToInt32(Console.ReadLine());
+            AsalSayı(number);
+
+        }
+
+        public static int AsalSayı(int number)
+        {
+            
+            bool asal = true;
+            for (int i = 2; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    Console.WriteLine("Asal Değil");
+                    asal = false;
+                    break;
+                }
+
+            }
+
+            if (asal == true)
+            {
+                Console.WriteLine("Asal");
+
+            }
+
+
+            Console.ReadLine();
+            return number;
+        }
+    }
+}
